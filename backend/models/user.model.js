1101f5183fe0890ed426b719a4f5a8f373b9366b
Jerry Thomas <jerry.l.thomas@stonybrook.edu>
2021-03-31 17:57:11 -0400

    Added user.model.js

---
+++ backend/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        minLength: 1,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        minLength: 8,
+        required: true
+    }
+}, {
+    timestamps: true,
+});
