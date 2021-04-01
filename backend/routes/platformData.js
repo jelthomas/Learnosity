f1ff8c1d31bcf6458206c919d8f7d1379cabfc21
ChosenHero <dnnywong123@gmail.com>
2021-04-01 18:04:25 -0400

    Added more routes to platformData.js (#6)

--- backend/routes/platformData.js
+++ backend/routes/platformData.js
@@ -1,24 +1,35 @@
 const router = require('express').Router();
 let platformData = require('../models/platformData.model');
 
+//gets every platform Data
 router.route('/').get((req, res) => {
   platformData.find()
     .then(platformDatas => res.json(platformDatas))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+//get platform Data's using array of Learned Platform Data
+router.route('/getLearnedPlatforms').get((req, res) => {
+  platformData.find({ _id: {$in : req.body.platformDatas_id}})
+    .then(platformDatas => res.json(platformDatas))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//gets a platform Data based on ID
 router.route('/:id').get((req, res) => {
   platformData.findById(req.params.id)
     .then(platformData => res.json(platformData))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+//deletes a platform Data based on ID
 router.route('/:id').delete((req, res) => {
   platformData.findByIdAndDelete(req.params.id)
       .then(() => res.json('Platform Data deleted.'))
       .catch(err => res.status(400).json('Error: ' + err));
 });
 
+//adds a platform Data entry 
 router.route('/add').post((req, res) => {
   const user_id = req.body.user_id;
   const platform_id = req.body.platform_id;
@@ -41,7 +52,7 @@ router.route('/add').post((req, res) => {
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
-
+//updates the platform Data by ID
 router.route('/update/:id').post((req, res) => {
   platformData.findById(req.params.id)
       .then(platformData => {
@@ -53,6 +64,17 @@ router.route('/update/:id').post((req, res) => {
         platformData.recently_played = req.body.recently_played;
           platformData.save()
               .then(() => res.json('Platform Data Updated!'))
+              .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/updatePages/:id').post((req, res) => {
+  platformData.findById(req.params.id)
+      .then(platformData => {
+        platformData.completed_pages = req.body.completed_pages;
+          platformData.save()
+              .then(() => res.json('Platform Data Pages Updated!'))
               .catch(err => res.status(400).json('Error: ' + err));
       })
       .catch(err => res.status(400).json('Error: ' + err));
