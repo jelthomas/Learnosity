1ffcf82d4066eb4a10478fdb6ba78ee844ab667b
Jerry Thomas <jerry.l.thomas@stonybrook.edu>
2021-04-06 19:35:26 -0400

    Added backend_delete.test.js

---
+++ backend_delete.test.js
@@ -0,0 +1,31 @@
+const {MongoClient} = require('mongodb');
+
+describe('delete', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    db = await connection.db(global.__MONGO_DB_NAME__);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
+
+  it('should delete a doc from the collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {username: 'TestJohn'};
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({username: 'TestJohn'});
+    await users.delete(mockUser);
+    const user = await users.find({username: 'TestJohn'});
+    expect(user).toEqual([]);
+  });
+});
