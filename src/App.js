b69409470bc5d0fe07c52e8d3504ec0429183846
Jerry Thomas <48896308+jelthomas@users.noreply.github.com>
2021-04-06 16:54:38 -0400

    Added Home Component in home.component.js (#15)
    * Added Home Component in home.component.js
    
    * Reinstalled node modules and added Home Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./components/home.component";
+// import Navbar from "./components/navbar.component";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      HELLO Robert!!!
-    </div>
-  );
+    <Router>
+      <div className="container">
+        {/* <Navbar />
+        <br/> */}
+        <Route path="/" exact component={Home} />
+      </div>
+    </Router>
+  ); 
 }
 
 export default App;
