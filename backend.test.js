5b745f36a97e71e66011b260915933dffa8b0a63
Danny <dnnywong123@gmail.com>
2021-04-06 18:15:43 -0400

    added a backend test

---
+++ backend.test.js
@@ -0,0 +1,29 @@
+const {MongoClient} = require('mongodb');
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    db = await connection.db(global.__MONGO_DB_NAME__);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
+
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {username: 'TestJohn'};
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({username: 'TestJohn'});
+    expect(insertedUser).toEqual(mockUser);
+  });
+});
