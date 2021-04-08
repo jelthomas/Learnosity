41eca2f46a311584c1e4cb69892f04a4091a432e
Danny <dnnywong123@gmail.com>
2021-04-08 13:12:12 -0400

    fixed backend.test.js to work with mongodb server

--- backend.test.js
+++ backend.test.js
@@ -1,15 +1,19 @@
 const {MongoClient} = require('mongodb');
+const ATLAS_URI= "mongodb+srv://jelthomas:learnosity_proj@learnositydata.hvfbn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//import axios from 'axios';
+
+const user = require('./backend/models/user.model');
 
 describe('insert', () => {
   let connection;
   let db;
 
   beforeAll(async () => {
-    connection = await MongoClient.connect(global.__MONGO_URI__, {
+    connection = await MongoClient.connect(ATLAS_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    db = await connection.db(global.__MONGO_DB_NAME__);
+    db = await connection.db("TestCollection");
   });
 
   afterAll(async () => {
@@ -17,13 +21,33 @@ describe('insert', () => {
     await db.close();
   });
 
-  it('should insert a doc into collection', async () => {
+  it('should insert a User into collection', async () => {
     const users = db.collection('users');
 
-    const mockUser = {username: 'TestJohn'};
-    await users.insertOne(mockUser);
+    const username = "Test Name";
+    const email = "test@gmail.com";
+    const password = "test_password";
+    const security_question = "What was the name of your first pet?";
+    const security_answer = "Rex";
+    const total_time_played = 0;
+    const completed_platforms = 0;
+    const experience_points = 0;
 
-    const insertedUser = await users.findOne({username: 'TestJohn'});
-    expect(insertedUser).toEqual(mockUser);
+    const newUser = new user({
+      username, 
+      email,
+      password,
+      security_question,
+      security_answer,
+      total_time_played,
+      completed_platforms,
+      experience_points
+    });
+
+    await users.insertOne(newUser);
+
+    const insertedUser = await users.findOne({username:'test name'});
+    // console.log(insertedUser)
+    expect(insertedUser.username).toEqual(newUser.username);
   });
 });
