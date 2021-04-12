ca75a2eaf23ee0cef40bcb053fea83b8badaffa9
Jerry Thomas <48896308+jelthomas@users.noreply.github.com>
2021-04-11 21:14:03 -0400

    Added main framework for login.component.js. Still needs a bit of css tweaking and Axios call. Also added forgot_password.component.js (#36)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/home.component";
 import SignUp from "./components/signup.component";
 import Login from "./components/login.component";
 import Navbar from "./components/navbar.component";
+import ForgotPassword from "./components/forgot_password.component";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={SignUp}/>
         <Route path="/login" exact component={Login} />
+        <Route path="/forgot" exact component={ForgotPassword} />
       </div>
     </Router>
   ); 
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -13,7 +13,7 @@ export default class Navbar extends Component {
         <div className="collpase navbar-collapse ">
         <ul className="navbar-nav mr-auto"> 
           <li className="navbar-item">
-            <p class="font-weight-normal" style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px',paddingLeft:'750px',paddingRight:'770px'}} > Learnosity</p>
+            <p class="font-weight-normal" style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px'}} > Learnosity</p>
           </li>
           <li className="navbar-item">
             <Link to="/signup" className="nav-link" style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px'}}>Sign Up</Link>
