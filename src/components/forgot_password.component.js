89336f68d67cadb1dfc3ed6a18db7da955e938f8
Jerry Thomas <jerry.l.thomas@stonybrook.edu>
2021-04-11 21:11:42 -0400

    Added main framework for login.component.js. Still needs a bit of css tweaking and Axios call. Also added forgot_password.component.js

---
+++ src/components/forgot_password.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {api} from "../axios_api.js";
+
+export default class ForgotPassword extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    
+
+
+    render() {
+        return (
+            <div>
+                Forgot Password Screen
+            </div>
+        )
+    }
+}
