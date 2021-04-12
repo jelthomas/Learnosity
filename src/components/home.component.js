fb6f180ae9732a6cbc343a11edf7c15b24608f92
ChosenHero <dnnywong123@gmail.com>
2021-04-12 15:33:58 -0400

    fixed issues with bootstrap and added some content to signup (#37)

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import {api} from "../axios_api.js";
+import Carousel from 'react-bootstrap/Carousel'
 import Carousel1 from "../images/Carousel1.jpg"
 import Logo from "../images/LearnLogo.png"
 import Penguin from "../images/Penguin.jpg"
@@ -29,55 +30,8 @@ export default class Home extends Component {
 
     render() {
         return (
-            // <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            //     <div class="carousel-inner">
-            //         <div class="carousel-item active">
-            //         <img src={Carousel1} class="d-block w-100" alt="..."/>
-            //         </div>
-            //         <div class="carousel-item">
-            //         <img src={Logo} class="d-block w-100" alt="..."/>
-            //         </div>
-            //         <div class="carousel-item">
-            //         <img src={Penguin}class="d-block w-100" alt="..."/>
-            //         </div>
-            //     </div>
-            //     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            //         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            //         <span class="visually-hidden">Previous</span>
-            //     </button>
-            //     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            //         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            //         <span class="visually-hidden">Next</span>
-            //     </button>
-            // </div>
-        //     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        //     <div class="carousel-indicators">
-        //       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        //       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        //       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        //     </div>
-        //     <div class="carousel-inner">
-        //       <div class="carousel-item active">
-        //         <img src={Carousel1} class="d-block w-100" alt="..."/>
-        //       </div>
-        //       <div class="carousel-item">
-        //         <img src={Logo} class="d-block w-100" alt="..."/>
-        //       </div>
-        //       <div class="carousel-item">
-        //         <img src={Logo} class="d-block w-100" alt="..."/>
-        //       </div>
-        //     </div>
-        //     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        //       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        //       <span class="visually-hidden">Previous</span>
-        //     </button>
-        //     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        //       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        //       <span class="visually-hidden">Next</span>
-        //     </button>
-        //   </div>
             <div class="container">
-                <div id="homeCarousel" class="carousel slide" data-bs-ride="carousel">
+                {/* <div id="homeCarousel" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
                         <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -102,7 +56,31 @@ export default class Home extends Component {
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
-                </div> 
+                </div>  */}
+
+                <Carousel>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={Carousel1}
+                    alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={Logo}
+                    alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={Penguin}
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+                </Carousel>
             </div>
         )
     }
--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,16 +1,82 @@
 import React, { Component } from 'react';
 //import {api} from "../axios_api.js";
-
+import { Link } from 'react-router-dom';
+import Logo from "../images/LearnLogo.png"
+import Form from "react-bootstrap/Form"
 export default class SignUp extends Component {
     constructor(props){
         super(props);
 
+        this.onChange = this.onChange.bind(this);
+        this.state = {
+            username: '',
+            email:'',
+            password: '',
+            confirmPassword:''
+        }
     }
+
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value})
+        console.log("TESTING " + e.target.name + " " + e.target.value)
+    }
+
 
     render() {
         return (
-            <div>
-                Sign Up
+                <div style={{background: "rgb(59, 59, 59)"}}>
+                
+                <div className ="container">
+                    <div className = "row">
+                        <div className = "col-md-6 mt-5 mx-auto">
+                            <div style = {{backgroundColor: "white", padding: "0px 20px 20px 20px", borderStyle: "solid", borderRadius: "28px"}}> 
+                                <div style = {{textAlign: "center", color: 'rgb(0, 219, 0)', width: "max-content", margin: "auto", fontSize: "55px", padding: "3px"}}>
+                                    <Link to="/" className="navbar-brand">
+                                        <img width = {60} src = {Logo} alt =""/>
+                                    </Link>
+                                    Learnosity
+                                </div>
+                                <div className = "form-group" style={{marginLeft: "10%"}}>
+                                    <label style = {{color: "black"}}> Username:</label>
+                                    <input type = "username" style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "username" placeholder = "Username" value = {this.state.username} onChange = {this.onChange} required/>
+                                </div>
+                                <div className = "form-group" style={{marginLeft: "10%"}}>
+                                    <label style = {{color: "black"}}> Password:</label>
+                                    <input type = "password" style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "password" placeholder = "Password" value = {this.state.password} onChange = {this.onChange} required/>
+                                </div>
+                                <div className = "form-group" style={{marginLeft: "10%"}}>
+                                    <label style = {{color: "black"}}> Confirm Password:</label>
+                                    <input type = "password" style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "confirmPassword" placeholder = "Enter Password Again" value = {this.state.confirmPassword} onChange = {this.onChange} required/>
+                                </div>
+                                <Form style={{marginLeft: "10%",marginRight: "10%"}}>
+                                    <Form.Group controlId="exampleForm.ControlSelect1">
+                                        <Form.Label>Security Question</Form.Label>
+                                        <Form.Control as="select">
+                                        <option>What was your first car?</option>
+                                        <option>What was the name of your first pet?</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Form>
+                                {/* <div className = "form-group" style={{marginLeft: "10%"}}>
+                                    <label style = {{color: "black"}}> Username:</label>
+                                    <input type = "email" style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "identifier" placeholder = "Email address or username" value = {this.state.identifier} onChange = {this.onChange} required/>
+                                </div>
+                                <div className = "form-group" style={{marginLeft: "10%"}}>
+                                    <label style = {{color: "black"}}> Password:</label>
+                                    <input type = "password" style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "password" placeholder = "Password" value = {this.state.password} onChange = {this.onChange} required/>
+                                </div>
+                                <Link to="/forgot" style={{color: "blue", justifyContent: "center", display: "flex"}}>Forgot Password?</Link>
+                                <button onClick={this.handleLogin} style = {{margin: "auto", marginTop: "10px", display: "block", backgroundColor: "limegreen", fontSize: "25px", borderStyle: "solid", borderRadius: "20px", borderColor: "grey", borderWidth: "1px", width: "45%", paddingBottom: "1.5%", paddingTop: "0.5%", color: "white"}}>
+                                    Login
+                                </button>
+                                <div style = {{color: "black", textAlign: "center", fontSize: "15px", marginTop: "10px"}} className = "form-group">
+                                    Don't have an account?
+                                </div>
+                                <Link to="/signup" className="btn btn-primary" style = {{justifyContent: "center", display: "flex", color: "black", background: "white", width: "45%", borderStyle: "solid", borderRadius: "20px", margin: "auto", fontSize: "18px", borderColor: "grey"}}>Sign Up</Link> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
