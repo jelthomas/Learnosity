c226e9db013ef536a51bfd8d419b64ae8269ea87
Jerry Thomas <48896308+jelthomas@users.noreply.github.com>
2021-04-14 18:58:51 -0400

    Fixed login css. Converted login to form in order to validate inputs (#45)

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import {api} from "../axios_api.js";
 import jwt from 'jsonwebtoken';
 import { Link } from 'react-router-dom';
-import Logo from "../images/LearnLogo.png"
-import jwt_decode from 'jwt-decode'
+import Logo from "../images/LearnLogo.png";
+import jwt_decode from 'jwt-decode';
+import "../format.css";
 
 export default class Login extends Component {
     constructor(props){
@@ -15,7 +16,7 @@ export default class Login extends Component {
         this.state = {
             identifier: '',
             password: '',
-            message: 'Incorrect Password!'
+            message: ''
         }
     }
 
@@ -70,7 +71,7 @@ export default class Login extends Component {
     }
 
     onChange(e){
-        this.setState({[e.target.name]: e.target.value})
+        this.setState({[e.target.name]: e.target.value, message:''})
       }
 
     handleLogin(e){
@@ -109,38 +110,33 @@ export default class Login extends Component {
 
     render() {
         return (
-            <div style={{background: "rgb(59, 59, 59)"}}>
-                
-                <div className ="container">
-                    <div className = "row">
-                        <div className = "col-md-6 mt-5 mx-auto">
-                            <div style = {{backgroundColor: "white", padding: "0px 20px 20px 20px", borderStyle: "solid", borderRadius: "28px"}}> 
-                                <Link to="/" className="navbar-brand">
-                                    <img width = {60} src = {Logo} alt =""/>
-                                </Link>
-                                <div style = {{textAlign: "center", color: 'rgb(0, 219, 0)', width: "max-content", margin: "auto", fontSize: "55px", padding: "3px"}}>
-                                    Learnosity
-                                </div>
-                                <div style = {{color: "red", textAlign: "center", fontSize: "25px"}}>{this.state.message}</div>
-                                <div className = "form-group" style={{marginLeft: "10%"}}>
-                                    <label style = {{color: "black"}}> Username:</label>
-                                    <input type = "email" style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "identifier" placeholder = "Email address or username" value = {this.state.identifier} onChange = {this.onChange} required/>
-                                </div>
-                                <div className = "form-group" style={{marginLeft: "10%"}}>
-                                    <label style = {{color: "black"}}> Password:</label>
-                                    <input type = "password" style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "password" placeholder = "Password" value = {this.state.password} onChange = {this.onChange} required/>
-                                </div>
-                                <Link to="/forgot" style={{color: "blue", justifyContent: "center", display: "flex"}}>Forgot Password?</Link>
-                                <button onClick={this.handleLogin} style = {{margin: "auto", marginTop: "10px", display: "block", backgroundColor: "limegreen", fontSize: "25px", borderStyle: "solid", borderRadius: "20px", borderColor: "grey", borderWidth: "1px", width: "45%", paddingBottom: "1.5%", paddingTop: "0.5%", color: "white"}}>
-                                    Login
-                                </button>
-                                <div style = {{color: "black", textAlign: "center", fontSize: "15px", marginTop: "10px"}} className = "form-group">
-                                    Don't have an account?
-                                </div>
-                                <Link to="/signup" className="btn btn-primary" style = {{justifyContent: "center", display: "flex", color: "black", background: "white", width: "45%", borderStyle: "solid", borderRadius: "20px", margin: "auto", fontSize: "18px", borderColor: "grey"}}>Sign Up</Link>
-                            </div>
+            <div className ="custom_container" style={{background: "rgb(59, 59, 59)"}}>
+                <div className = "custom_col mt-5 mx-auto">
+                    <form onSubmit={this.handleLogin} style = {{backgroundColor: "white", padding: "0px 20px 20px 20px", borderStyle: "solid", borderRadius: "28px"}}> 
+                        <Link to="/" className="navbar-brand" id="image">
+                            <img width = {60} src = {Logo} alt =""/>
+                        </Link>
+                        <div style = {{textAlign: "center", color: 'rgb(0, 219, 0)', width: "max-content", margin: "auto", fontSize: "55px", padding: "3px", marginTop:"-75px"}}>
+                            Learnosity
                         </div>
-                    </div>
+                        <div style = {{color: "red", textAlign: "center", fontSize: "25px"}}>{this.state.message}</div>
+                        <div className = "form-group" style={{marginLeft: "10%"}}>
+                            <label style = {{color: "black"}}> Username:</label>
+                            <input style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "identifier" placeholder = "Email address or username" value = {this.state.identifier} onChange = {this.onChange} required/>
+                        </div>
+                        <div className = "form-group" style={{marginLeft: "10%"}}>
+                            <label style = {{color: "black"}}> Password:</label>
+                            <input type = "password" style = {{width: "90%", borderColor: "black"}} className = "form-control" name = "password" placeholder = "Password" value = {this.state.password} onChange = {this.onChange} required/>
+                        </div>
+                        <Link to="/forgot" style={{color: "blue", justifyContent: "center", display: "flex"}}>Forgot Password?</Link>
+                        <button type= "submit" style = {{margin: "auto", marginTop: "10px", display: "block", backgroundColor: "limegreen", fontSize: "25px", borderStyle: "solid", borderRadius: "20px", borderColor: "grey", borderWidth: "1px", width: "45%", paddingBottom: "1.5%", paddingTop: "0.5%", color: "white"}}>
+                            Login
+                        </button>
+                        <div style = {{color: "black", textAlign: "center", fontSize: "15px", marginTop: "10px"}} className = "form-group">
+                            Don't have an account?
+                        </div>
+                        <Link to="/signup" className="btn btn-primary" style = {{justifyContent: "center", display: "flex", color: "black", background: "white", width: "45%", borderStyle: "solid", borderRadius: "20px", margin: "auto", fontSize: "18px", borderColor: "grey"}}>Sign Up</Link>
+                    </form>
                 </div>
             </div>
         );
