80db60ef16f87aa65527427e39f1d3c8d1688bae
Danny <dnnywong123@gmail.com>
2021-04-15 20:01:21 -0400

    fixed up navbar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,24 +6,29 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-light bg-light navbar-expand-lg">
-        <Link to="/" className="navbar-brand">
-            <img width = {60} src = {Logo} alt =""/>
-        </Link>
-        <div className="collpase navbar-collapse ">
-        <ul className="navbar-nav mr-auto"> 
-          <li className="navbar-item">
-            <p class="font-weight-normal" style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px'}} > Learnosity</p>
-          </li>
-          <li className="navbar-item">
-            <Link to="/signup" className="nav-link" style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px'}}>Sign Up</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to ="/login" className="nav-link"  style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px'}}  >Log In</Link>
-          </li>
+
+      <nav class="navbar navbar-light bg-light navbar-expand-lg">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto">
+            <Link to="/" className="navbar-brand">
+                <img width = {60} src = {Logo} alt =""/>
+            </Link>
         </ul>
-        </div>
-      </nav>
+    </div>
+    <div class="mx-auto order-0">
+      <p class="font-weight-normal" style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px'}} > Learnosity</p>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li className="navbar-item">
+              <Link to="/signup" className="nav-link" style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px'}}>Sign Up</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to ="/login" className="nav-link"  style={{padding: '5px',color:'#00db00',fontFamily:"Quando",fontSize:'25px'}}  >Log In</Link>
+            </li>
+        </ul>
+    </div>
+</nav>
     );
   }
 }
