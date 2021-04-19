b3ee11d64f8924b905c3bf0d9ab860801ed23db9
Danny <dnnywong123@gmail.com>
2021-04-19 19:36:07 -0400

    favorite button works properly in dashboard

---
+++ src/components/useplatform.component.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import {api} from "../axios_api.js";
+import jwt from 'jsonwebtoken';
+import { Link } from 'react-router-dom';
+import Logo from "../images/LearnLogo.png";
+import jwt_decode from 'jwt-decode';
+import Card from 'react-bootstrap/Card'
+import { myObject } from "./forgot_password.component"
+import Navbar from "./navbar.component";
+
+export default class UsePlatform extends Component {
+    constructor(props){
+        super(props);
+        
+
+        this.state = {
+            user_id: ''
+        }
+    }
+
+    componentDidMount(){
+        var token = localStorage.getItem('usertoken');
+        var validToken = false;
+        if(token){
+            //Token in session storage
+            jwt.verify(token, "jwt_key", function(err,res) {
+                if(err){
+                    //Improper JWT format 
+                    //Remove token and redirect back to home
+                    localStorage.removeItem('usertoken');
+                }
+                else{
+                    //Properly formatted JWT
+                    validToken = true;
+                }});
+        }
+        if(validToken){
+            //Check if ID is in token and ID exists as a user
+            const decoded = jwt_decode(token);
+            if (decoded._id){
+                //ID exists in token
+                //Check if ID exists as a user
+                api.get('/user/'+ decoded._id)
+                .then(response => {
+                    if (response) {
+                        //Valid user
+                    }
+                    else{
+                        //Fake ID...
+                        localStorage.removeItem('usertoken');
+                        this.props.history.push(`/`);
+                    }
+                })
+                .catch(err => console.log("User Error: " + err));
+            }
+        }  
+        else{
+            //Not a Valid Token
+            localStorage.removeItem('usertoken');
+            this.props.history.push(`/`);
+        }
+
+        //check if user has a platform data for this platform format 
+        // console.log(this.props.location.pathname.substring(13))
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        {window.location.pathname}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+}
