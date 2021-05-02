4507f471a0edece06e07ef8597de300bd83ce4ab
ChosenHero <dnnywong123@gmail.com>
2021-05-02 18:21:47 -0400

    added basic click on platform and category (#99)

--- src/components/platform.component.js
+++ src/components/platform.component.js
@@ -3,9 +3,9 @@ import {api} from "../axios_api.js";
 import jwt from 'jsonwebtoken';
 import jwt_decode from 'jwt-decode'
 import "../format.css";
-// import Card from "react-bootstrap/Card"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faStar, faPlay, faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import Card from "react-bootstrap/Card"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faPlay, faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import DefaultCoverPhoto from "../images/defaultCoverPhoto.png"
 require('dotenv').config();
 
@@ -14,12 +14,23 @@ export default class Platform extends Component {
     constructor(props){
         super(props);
 
+        this.clickUseCategory = this.clickUseCategory.bind(this);
+
         this.state = {
             username: "",
             id: "",
-            platformFormat: ""
+            platformFormat: "",
+            categoriesFormats:[]
         }
 
+    }
+
+    clickUseCategory(cat_id) {
+
+        var platform_format_id = this.props.location.pathname.substring(10);
+
+
+        this.props.history.push("/usecategory/"+platform_format_id+"/"+cat_id);
     }
     
     
@@ -61,7 +72,21 @@ export default class Platform extends Component {
                         api.get('/platformFormat/getSpecificPlatformFormat/'+platform_format_id)
                         .then(response => {
                             console.log(response.data)
-							this.setState({platformFormat:response.data[0]})
+                            console.log(response.data[0].categories)
+							//this.setState({platformFormat:response.data[0]})
+
+                            const catFormatInfo = {
+                                categories_id : response.data[0].categories
+                            }
+
+                            //gets every category format inside of categories array
+                            api.post('/categoryFormat/getAllCategories/',catFormatInfo)
+                            .then(res => {
+                                this.setState({platformFormat:response.data[0],categoriesFormats:res.data})
+                            })
+                            .catch(err => {
+                                console.log(err.response)
+                            })
 
                         })
                         .catch(error => {
@@ -94,7 +119,22 @@ export default class Platform extends Component {
         return (
             <div>
                 <p>TESTING PLAT</p>
-                <p>{this.state.platformFormat._id}</p>
+                <p>{this.state.platformFormat.plat_name}</p>
+                {this.state.categoriesFormats.map((category, index) => (
+                    <Card className = "card_top itemsContainer">
+                    <FontAwesomeIcon className="play_button" icon={faPlay} />
+                    <Card.Img variant="top" onClick={() => this.clickUseCategory(category._id)} src={category.category_photo === "" ? DefaultCoverPhoto : category.category_photo} className = "card_image"/>
+                        <Card.Body className = "card_body">
+                            <Card.Title className = "card_info">{category.category_name}</Card.Title>
+                            {/* <Card.Text className = "card_info">
+                            {category.owner}
+                            </Card.Text> */}
+                            {/* <div style={{width: "fit-content"}} onClick={() => this.toggleFavoriteAll(index)}>
+                                <FavoriteButton isfavorited={platform.is_favorited}/>
+                            </div> */}
+                        </Card.Body>
+                    </Card>
+                ))}
             </div>
         )
     }
